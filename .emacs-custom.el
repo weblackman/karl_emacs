fa43be5c1e2350487ea3a87d34957932c39ba694
karl <karl@PC-200912121246.(none)>
2010-02-06 16:25:31 +0800

    emacs custom  config

---
+++ .emacs-custom.el
@@ -0,0 +1,50 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(Man-circular-pages-flag nil)
+ '(Man-downcase-section-letters-flag nil)
+ '(Man-fontify-manpage-flag nil)
+ '(ack-arguments (quote ("<nul")))
+ '(ack-context 1)
+ '(ack-executable (executable-find "ack"))
+ '(backpack-api-key "f6e6a7aeda10579712d0671b3d77dbe78438c158")
+ '(backpack-username "lss247")
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(display-time-mode t)
+ '(flymake-js-rhino-jar "d:/lib/javalib/rhino1_7R2/js.jar")
+ '(flymake-js-rhino-js "d:/lib/javalib/")
+ '(flymake-php-on t)
+ '(hl-paren-background-colors nil)
+ '(ido-enable-regexp t)
+ '(newsticker-url-list (quote (("Cakephp" "http://bakery.cakephp.org/articles/rss" nil nil nil) ("ftchinese" "http://www.ftchinese.com/rss/feed" nil nil nil))))
+ '(newsticker-url-list-defaults (quote (("Emacs Wiki" "http://www.emacswiki.org/cgi-bin/wiki.pl?action=rss" nil 3600) ("slashdot" "http://slashdot.org/index.rss" nil 3600))))
+ '(nxml-heading-element-name-regexp "title\\|head\\|span")
+ '(nxml-section-element-name-regexp "article\\|\\(sub\\)*section\\|chapter\\|div\\|appendix\\|part\\|preface\\|reference\\|simplesect\\|bibliography\\|bibliodiv\\|glossary\\|glossdiv\\|html\\|body\\|form")
+ '(nxml-slash-auto-complete-flag t)
+ '(popcmp-short-help-beside-alts nil)
+ '(recentf-max-saved-items 5)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(smex-prompt-string "$")
+ '(snipplr-user-key "70488e10c08fb5106b39")
+ '(sql-database "350115_clients")
+ '(twit-protocol "https")
+ '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#414141" :foreground "#F8F8F8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 120 :width normal :foundry "outline" :family "Envy Code R"))))
+ '(diredp-dir-priv ((t (:foreground "Green"))))
+ '(diredp-file-name ((t nil)))
+ '(flymake-errline ((((class color) (background dark)) (:underline t))))
+ '(fringe-bookmark-flag-face ((t (:foreground "yellow"))))
+ '(hl-paren-face ((t (:inverse-video t))) t)
+ '(moccur-face ((t (:background "#2f4f4f" :weight bold)))))
+
+
+
